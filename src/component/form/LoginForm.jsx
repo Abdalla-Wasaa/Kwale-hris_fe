54986361dfc1a62d751f0aca8cbf5b90f63b5159
Abdalla-Wasaa <abdallawasaa7@gmail.com>
2024-10-21 10:30:33 +0300

    logging the base url

--- src/component/form/LoginForm.jsx
+++ src/component/form/LoginForm.jsx
@@ -13,6 +13,8 @@ function LoginForm() {
   axios.defaults.withCredentials = true;
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
+  console.log("API_BASE_URL:", API_BASE_URL);
+
   function handleSubmit(e) {
     e.preventDefault();
     localStorage.setItem('token',null);
